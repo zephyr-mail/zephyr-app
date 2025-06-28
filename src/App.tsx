273334d40818cb19b063f8c3ec33a6fc4e9661fc
Oliver Saca <oliversaca2@gmail.com>
2025-06-28 14:09:11 +0200

    Prueba Incial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://google.com">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
